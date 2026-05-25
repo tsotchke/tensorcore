39ab2b80777a61c099cd13da3bd8db3094ac5f21
tsotchke <tsotchkele@gmail.com>
2026-05-24 21:32:06 -0400

    Fix MSVC C ABI marker

--- lib/c_api/c_api_cpu.c
+++ lib/c_api/c_api_cpu.c
@@ -4,5 +4,15 @@
 
 #include "tensorcore/tensorcore.h"
 
-__attribute__((visibility("default"), used))
+#if defined(_MSC_VER)
+#  define TC_ABI_MARKER_ATTR
+#elif defined(__GNUC__) || defined(__clang__)
+#  define TC_ABI_MARKER_ATTR __attribute__((visibility("default"), used))
+#else
+#  define TC_ABI_MARKER_ATTR
+#endif
+
+TC_ABI_MARKER_ATTR
 const int tensorcore_abi_version = 1;
+
+#undef TC_ABI_MARKER_ATTR
